608b7b84dd99f5b47d537aad18c494cac4caab2d
TizKizOy <zaxar.pro.gamer.killer@yandex.by>
2024-07-29 12:40:32 +0300

    Stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace MyFinallySteck
+{
+    class Beta
+    {
+        static void Main()
+        {
+
+            Stack stack = new Stack(7);
+            stack.PrintSteck();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Pop();
+            stack.Push(4);
+            stack.Peek();
+            stack.PrintSteck();
+            stack.Push(5);
+            stack.Push(6);
+            stack.Pop();
+            stack.Push(7);
+            stack.Peek();
+            stack.PrintSteck();
+            
+
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFinallySteck
+{
+    internal class Stack
+    {
+        private int[] _Array;
+        private readonly int _Size;
+        private int _Top;
+
+        public Stack()
+        {
+            this._Array = new int[0];
+            this._Size = 0;
+            this._Top = 0;
+        }
+
+        public Stack(int size)
+        {
+            this._Size = size;
+            this._Array = new int[size];
+            this._Top = 0;
+        }
+
+        private bool IsEmpty()
+        {
+            if (this._Top == 0)
+                return true;
+            else
+                return false;
+        }
+
+        private bool IsFull()
+        {
+            if (this._Array.Length == this._Top)
+                return true;
+            else
+                return false;
+        }
+
+        public void Push(int value)
+        {
+            if (IsFull())
+                Console.WriteLine("Стек переполнен.");
+            else
+            {
+                this._Array[this._Top] = value;
+                this._Top++;
+                Console.WriteLine($"Элемент: {value}, добавлен в стек.");
+            }
+        }
+
+        public void Pop()
+        {
+            if (IsEmpty())
+                Console.WriteLine("Стек пуст.");
+            else
+            {
+                Console.WriteLine($"Элемент: {this._Array[_Top - 1]}, извлекли и удалили из стека.");
+                this._Array[_Top] = 0;
+                this._Top--;
+            }
+        }
+
+        public void Peek()
+        {
+            if (IsEmpty())
+                Console.WriteLine("Стек пуст.");
+            else
+                Console.WriteLine($"Последний элемент стека: {this._Array[this._Top - 1]}");
+        }
+
+        public void PrintSteck()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Стек:");
+            Console.WriteLine("___");
+            for (int i = this._Array.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine("|" + this._Array[i] + "|");
+            }
+            Console.WriteLine("---");
+        }
+    }
+}
